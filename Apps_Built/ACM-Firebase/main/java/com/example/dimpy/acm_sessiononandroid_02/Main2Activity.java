336f8e38a81cc3488e7c292e255b0046629da9bc
dimpy-chhabra <dimpychhabra@yahoo.co.uk>
2018-01-20 00:36:44 +0530

    Resources Update

---
+++ Apps_Built/ACM-Firebase/main/java/com/example/dimpy/acm_sessiononandroid_02/Main2Activity.java
@@ -0,0 +1,20 @@
+package com.example.dimpy.acm_sessiononandroid_02;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Bundle extras = getIntent().getExtras();
+        String value1 = extras.getString("Value1");
+        String value2 = extras.getString("Value2");
+        Toast.makeText(getApplicationContext(),"Values are:\n First value: "+value1+
+                "\n Second Value: "+value2, Toast.LENGTH_LONG).show();
+    }
+}
